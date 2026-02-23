ef6bfb640887d3da78fe5b24042e8efb0db7cffa
mxrpheus6 <pkazachenkoo@gmail.com>
2026-02-23 15:27:51 +0300

    Configure keycloak
    - Add SecurityFilterChain and JwtAuthenticationConverter beans
    - Marked controller methods as PreAuthorize
    - Add required dependency

--- src/main/java/com/innowise/paymentservice/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/com/innowise/paymentservice/exception/advice/GlobalExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -59,6 +60,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.SERVICE_UNAVAILABLE)
                 .body(new ExceptionDto(LocalDateTime.now(), e.getMessage(), null));
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<?> handleResponseStatusException(ResponseStatusException e) {
+        return ResponseEntity
+                .status(e.getStatusCode()).build();
     }
 
     @ExceptionHandler(Exception.class)
